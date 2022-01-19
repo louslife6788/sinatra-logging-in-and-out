8ca0c15dbc0033649b7290aa894b85b1c5f60ad5
luis leon <leonluis380@yahoo.com>
2022-01-19 00:13:12 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,19 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect 'account'
+    end
+    erb :error
   end
 
+
+  
+
   get '/account' do
+    erb :account
 
   end
 
